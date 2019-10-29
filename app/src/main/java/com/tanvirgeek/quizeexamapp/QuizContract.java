75c6f6d0c78eb742c93b6ae94ee25949cb719fe3
tanvirgeek <tanvirgeek@gmail.comm>
2019-10-29 12:55:26 +0600

    QuizAPP day 2

---
+++ app/src/main/java/com/tanvirgeek/quizeexamapp/QuizContract.java
@@ -0,0 +1,18 @@
+package com.tanvirgeek.quizeexamapp;
+
+import android.provider.BaseColumns;
+
+public final class QuizContract {
+
+    private QuizContract(){}
+    public static class QuestionsTable implements BaseColumns {
+        public static final String TABLE_NAME = "quiz_questions";
+        public static final String COLUMN_QUESTIONS = "questions";
+        public static final String COLUMN_OPTION1= "options1";
+        public static final String COLUMN_OPTION2 = "options2";
+        public static final String COLUMN_OPTION3 = "options3";
+        public static final String COLUMN_OPTION4 = "options4";
+        public static final String COLUMN_ANSWERNO = "answers_no";
+
+    }
+}
