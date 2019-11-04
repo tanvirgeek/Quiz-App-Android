ac5f583ebecad828a436b5ecc0d52e5e16dc226b
tanvirgeek <tanvirgeek@gmail.comm>
2019-11-04 10:56:43 +0600

    w11D4

--- app/src/main/java/com/tanvirgeek/quizeexamapp/QuizActivity.java
+++ app/src/main/java/com/tanvirgeek/quizeexamapp/QuizActivity.java
@@ -2,13 +2,144 @@ package com.tanvirgeek.quizeexamapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Collections;
+import java.util.List;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private TextView textViewQuestion;
+    private TextView textViewScore;
+    private TextView textViewQuestionCount;
+    private TextView textViewCountDown;
+    private RadioGroup rbGroup;
+    private RadioButton rb1;
+    private RadioButton rb2;
+    private RadioButton rb3;
+    private RadioButton rb4;
+    private Button buttonConfirmNext;
+    private List<Question> questionList;
+
+    private ColorStateList textcolorDefaultRB;
+
+    private int questionCounter;
+    private int questionCountTotal;
+    private Question currentQuestion;
+    private int score;
+    private boolean answered;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        textViewQuestion = findViewById(R.id.text_view_question);
+        textViewScore = findViewById(R.id.text_view_score);
+        textViewQuestionCount = findViewById(R.id.text_view_question_count);
+        textViewCountDown = findViewById(R.id.text_view_count_down);
+        rbGroup = findViewById(R.id.radioGroup);
+        rb1 = findViewById(R.id.radioButton1);
+        rb2 = findViewById(R.id.radioButton2);
+        rb3 = findViewById(R.id.radioButton3);
+        rb4 = findViewById(R.id.radioButton4);
+        buttonConfirmNext = findViewById(R.id.button_confirm_text);
+        textcolorDefaultRB = rb1.getTextColors();
+        QuizDbHelper dbHelper = new QuizDbHelper(this);
+        questionList = dbHelper.getAllQuestions();
+
+        questionCountTotal = questionList.size();
+        Collections.shuffle(questionList);
+        showNextQuestion();
+
+        buttonConfirmNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!answered){
+                    if(rb1.isChecked() || rb2.isChecked() || rb3.isChecked() || rb4.isChecked()  ){
+                        checkAnswer();
+                    }else {
+                        Toast.makeText(QuizActivity.this,"Please, Select an answer", Toast.LENGTH_SHORT).show();
+                    }
+                }else {
+                    showNextQuestion();
+                }
+            }
+        });
+    }
+
+    private void checkAnswer(){
+        answered = true;
+        RadioButton rbSelected = findViewById(rbGroup.getCheckedRadioButtonId());
+        int answerNr = rbGroup.indexOfChild(rbSelected) + 1;
+        if (answerNr == currentQuestion.getAnswerNo()){
+            score++;
+            textViewScore.setText("Score:" + score);
+        }
+
+        showSolution();
+    }
+
+    private void showSolution(){
+        rb1.setTextColor(Color.RED);
+        rb2.setTextColor(Color.RED);
+        rb3.setTextColor(Color.RED);
+
+        switch (currentQuestion.getAnswerNo()){
+            case 1:
+                rb1.setTextColor(Color.GREEN);
+                textViewQuestion.setText("Answer1 is correct");
+                break;
+            case 2:
+                rb1.setTextColor(Color.GREEN);
+                textViewQuestion.setText("Answer2 is correct");
+                break;
+
+            case 3:
+                rb1.setTextColor(Color.GREEN);
+                textViewQuestion.setText("Answer3 is correct");
+                break;
+            case 4:
+                rb1.setTextColor(Color.GREEN);
+                textViewQuestion.setText("Answer4 is correct");
+                break;
+        }
+        if(questionCounter<questionCountTotal){
+            buttonConfirmNext.setText("Next");
+        }else{
+            buttonConfirmNext.setText("Finish");
+        }
+    }
+
+    private void showNextQuestion(){
+        rb1.setTextColor(textcolorDefaultRB);
+        rb2.setTextColor(textcolorDefaultRB);
+        rb3.setTextColor(textcolorDefaultRB);
+        rb4.setTextColor(textcolorDefaultRB);
+        rbGroup.clearCheck();
+
+        if(questionCounter < questionCountTotal){
+            currentQuestion = questionList.get(questionCounter);
+
+            textViewQuestion.setText(currentQuestion.getQuestion());
+            rb1.setText(currentQuestion.getOption1());
+            rb2.setText(currentQuestion.getOption1());
+            rb3.setText(currentQuestion.getOption1());
+            rb4.setText(currentQuestion.getOption1());
+
+            questionCounter ++;
+            textViewQuestionCount.setText("Question: " + questionCounter + "/" + questionCountTotal);
+            answered = false;
+            buttonConfirmNext.setText("Confirm");
+        }else{
+            finish();
+        }
     }
 }
